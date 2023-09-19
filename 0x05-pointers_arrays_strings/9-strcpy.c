cf53261a6f0d97f60951aedf4517b4c8dd8b7a9b
BAsmAlA67 <basmalaelshabrawy37@gmail.com>
2023-09-19 16:28:08 -0700

    UPDate

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *_strcpy - Copy a string 
- * @dest: Destination value
- * @src: Source value
- * Return: the pointer to dest
+ *   _strcpy - Copy a string 
+ *   @dest: Destination value
+ *   @src: Source value
+ *   Return: the pointer to dest
  */
 
 char *_strcpy(char *dest, char *src)
