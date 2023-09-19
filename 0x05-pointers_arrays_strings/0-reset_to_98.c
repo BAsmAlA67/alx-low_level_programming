722686c742f458febcfcf81a8b72e95e5b6cf66a
BAsmAlA67 <basmalaelshabrawy37@gmail.com>
2023-09-19 07:06:12 -0700

    UPdate

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,9 +3,10 @@
  * reset_to_98 - takes a pointer to an int parameter
  * and updates the value it points to 98
  * @n: input
- * Return:n 
+ * Return: n 
  */
 void reset_to_98(int *n)
 {
 	*n = 98;
 }
+
