6abf99b23cd780935d3b1ec1404478f8dc71c6dc
BAsmAlA67 <basmalaelshabrawy37@gmail.com>
2023-09-21 19:39:59 -0700

    pip

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,18 +1,25 @@
 #include "main.h"
 
 /**
- * _strcmp Compares pointers to two string
+ * _strcmp - Compares pointers to two string
  * @s1: A pointer to the first string to be compared.
  * @s2: A pointer to the second string to be compared.
- * Return: s1[i] - s2[i]
+ * Return: 1 if true, 0 if false
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2 && *s1 == *s2)
+	int e = 0;
+
+	while (*s1)
 	{
+		if (*s1 != *s2)
+		{
+			e = ((int)*s1 - 48) - ((int)*s2 - 48);
+			break;
+		}
 		s1++;
 		s2++;
 	}
-	return (*s1 - *s2);
+	return (e);
 }
